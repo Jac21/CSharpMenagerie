13e5a1855adec2efdc0e16aa52f41e84a9bcf370
Jeremy Cantu <mail@jeremycantu.com>
2020-07-18 15:09:41 -0500

    Magical Methods custom awaitable implementation

---
+++ Experiments/MagicalMethods/MagicalMethods/CustomAwaiter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MagicalMethods
+{
+    // Custom awaitable types
+    public class CustomAwaiter : INotifyCompletion
+    {
+        public void OnCompleted(Action continuiation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CustomAwaitable
+    {
+        public CustomAwaiter GetAwaiter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
