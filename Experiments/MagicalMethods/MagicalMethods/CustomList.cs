4d62e6858949d373bd1c264e4ba5010e11a0579c
Jeremy Cantu <mail@jeremycantu.com>
2020-07-18 14:59:20 -0500

    Added Magical Methods experimental project, custom list syntax implementation

---
+++ Experiments/MagicalMethods/MagicalMethods/CustomList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MagicalMethods
+{
+    // Collection initialization syntax
+    public class CustomList<T> : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Collection initialization syntax with multiple parameters
+    public class CustomListWithMultipleParameters<T> : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item, string extraParameterOne, int extraParameterTwo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Collection initialization syntax with dynamic parameters
+    public class CustomListWithDynamicParameters<T> : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(IEnumerable<T> items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Adding collection initializer support for existing types
+    public static class ExistingTypeExtensions
+    {
+        public static void Add<T>(ExistingType @this, T item) => throw new NotImplementedException();
+    }
+
+    public class ExistingType
+    {
+    }
+}
---
+++ Experiments/MagicalMethods/MagicalMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MagicalMethods
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, Magical Methods!");
+
+            var customList = new CustomList<int>
+            {
+                1, 1, 2, 3, 5, 8
+            };
+
+            var customListWithMultipleParameters = new CustomListWithMultipleParameters<string>
+            {
+                {"item1", "extraParamOne", 2},
+                {"item2", "extraParamOne", 2}
+            };
+
+            var myList = new List<int>
+            {
+                1, 2, 3, 4
+            };
+
+            var customListWithDynamicParameters = new CustomListWithDynamicParameters<int>
+            {
+                myList
+            };
+        }
+    }
+}
