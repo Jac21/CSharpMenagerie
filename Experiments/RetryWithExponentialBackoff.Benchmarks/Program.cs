96bc3ed70ec0ff832be63774ec4471b9e1471012
Jeremy Cantu <mail@jeremycantu.com>
2021-04-07 22:16:07 -0500

    RetryWithExponentialBackoff minor unit test updates, benchmarks project creation

---
+++ Experiments/RetryWithExponentialBackoff.Benchmarks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace RetryWithExponentialBackoff.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+
+    public class RetryWithExponentialBackoffBenchmarks
+    {
+        private readonly RetryWithExponentialBackoff _retryWithExponentialBackoff = new RetryWithExponentialBackoff();
+
+        [Benchmark]
+        public async Task RetryDelayBenchmark()
+        {
+            await _retryWithExponentialBackoff.RunAsync(async () => { await Task.Delay(1); });
+        }
+    }
+}
