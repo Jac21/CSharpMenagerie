6e75743a95af6097110249d1f601aaf92b1d9806
Jeremy Cantu <mail@jeremycantu.com>
2021-04-28 20:47:52 -0500

    DotMemoryUnit tests

--- Reference/DotMemoryUnit.Unit.Tests/UnitTest1.cs
+++ Reference/DotMemoryUnit.Unit.Tests/UnitTest1.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+using JetBrains.dotMemoryUnit;
 using NUnit.Framework;
 
 namespace DotMemoryUnit.Unit.Tests
 {
-    public class Tests
+    public class DotMemoryUnitTests
     {
         [SetUp]
         public void Setup()
@@ -10,9 +12,35 @@ namespace DotMemoryUnit.Unit.Tests
         }
 
         [Test]
-        public void Test1()
+        public void CheckForObjects_Test()
         {
-            Assert.Pass();
+            dotMemory.Check(memory =>
+            {
+                Assert.That(memory.GetObjects(where => where.Type
+                        .Is<string>())
+                    .ObjectsCount, Is.LessThanOrEqualTo(10_000));
+            });
+        }
+
+        [DotMemoryUnit(CollectAllocations = true)]
+        [Test]
+        public void CheckMemoryTraffic_Test()
+        {
+            var memoryCheckPointOne = dotMemory.Check();
+
+            var stringList = new List<string>();
+
+            for (var i = 0; i < 10_000; i++)
+            {
+                stringList.Add(i.ToString());
+            }
+
+            var memoryCheckPointTwo = dotMemory.Check(memory =>
+            {
+                Assert.That(memory.GetTrafficFrom(memoryCheckPointOne)
+                    .Where(obj => obj.Type.Is<List<string>>())
+                    .AllocatedMemory.SizeInBytes, Is.LessThan(2048));
+            });
         }
     }
 }
