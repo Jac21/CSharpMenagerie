0707647b8be430c64bea74183ba1937f769ae023
Jeremy Cantu <mail@jeremycantu.com>
2021-05-31 18:04:53 -0500

    Synchronization Context reference project

---
+++ Reference/SynchronizationContext/MySynchronizationContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security;
+using System.Threading;
+
+namespace SynchronizationContext
+{
+    public class MySynchronizationContext : System.Threading.SynchronizationContext
+    {
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            Console.WriteLine($"Continuation dispatched to {nameof(MySynchronizationContext)}");
+
+            d.Invoke(state);
+        }
+
+        // Capturing the synchronization context
+
+        //[SecurityCritical]
+        //internal void SetContinuationForAwait(Action continuationAction, bool continueOnCapturedContext,
+        //    bool flowExecutionContext, ref StackCrawlMark stackMark)
+        //{
+        //    TaskContinuation tc = null;
+
+        //    if (continueOnCapturedContext)
+        //    {
+        //        var syncCtx = System.Threading.SynchronizationContext.Current;
+
+        //        if (syncCtx != null &&
+        //            syncCtx.GetType() != typeof(System.Threading.SynchronizationContext))
+        //        {
+        //            tc = new SynchronizationContextAwaitTaskContinuation(syncCtx, continuationAction,
+        //                flowExecutionContext, ref stackMark);
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Reference/SynchronizationContext/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SynchronizationContext
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            WriteLine("Current Synchronization Context: " +
+                      $"{System.Threading.SynchronizationContext.Current}");
+
+            System.Threading.SynchronizationContext
+                .SetSynchronizationContext(new MySynchronizationContext());
+
+            WriteLine("Current Synchronization Context: " +
+                      $"{System.Threading.SynchronizationContext.Current}");
+
+            await Task.Delay(100);
+
+            WriteLine("Completed!");
+
+            ReadLine();
+        }
+
+        /// <summary>
+        /// A default Synchronization Context implmentation
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="state"></param>
+        public virtual void Post(SendOrPostCallback d, object state)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(d), state);
+        }
+
+        /// <summary>
+        /// Example event handler, method will pause on await, attach
+        /// a continuation to the task awaiter. UI apps guarantee
+        /// that async continuations run on the UI thread by
+        /// implementing their own context/Post method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btn1_Click(object sender, EventArgs e)
+        {
+            await Task.Delay(1000);
+
+            // myControl.text = “After Await”;
+        }
+    }
+}
