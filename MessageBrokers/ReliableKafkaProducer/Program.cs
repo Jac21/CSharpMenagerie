e97c8e5800bd8a4510cee53509c33653dcb4a517
Jeremy Cantu <mail@jeremycantu.com>
2021-07-11 21:34:47 -0500

    Message broker folder creation, Kafka project and producer initial commit

---
+++ MessageBrokers/ReliableKafkaProducer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReliableKafkaProducer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Kafka!");
+        }
+    }
+}
