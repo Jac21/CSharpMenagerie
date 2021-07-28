557d64087aa8bc2a1a671f7c238a05146ba45864
Jeremy Cantu <mail@jeremycantu.com>
2021-07-28 18:21:20 -0500

    Added Option<T> example consumer

---
+++ Functional/MyFunctionalLibraryConsumers/Optionals/OptionalGreeter.cs
@@ -0,0 +1,13 @@
+﻿using MyFunctionalLibrary.Option;
+
+namespace MyFunctionalLibraryConsumers.Optionals
+{
+    public class OptionalGreeter
+    {
+        public string Greet(Option<string> greetee) =>
+            greetee.Match(() => "Sorry, who?", name => $"Hello, {name}");
+
+        public string GreetingFor(Subscriber subscriber) =>
+            subscriber.Name.Match(() => "Dear Subsriber,", name => $"Dear {name.ToUpperInvariant()}");
+    }
+}
---
+++ Functional/MyFunctionalLibraryConsumers/Optionals/Subscriber.cs
@@ -0,0 +1,14 @@
+﻿using MyFunctionalLibrary.Option;
+
+namespace MyFunctionalLibraryConsumers.Optionals
+{
+    public class Subscriber
+    {
+        /// <summary>
+        /// Explicitly marked as optional
+        /// </summary>
+        public Option<string> Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
