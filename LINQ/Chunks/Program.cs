600249db4a45433d15a8b75549ad6da4a103c893
Jeremy Cantu <mail@jeremycantu.com>
2021-10-17 13:04:58 -0500

    Project consolidation, Chunk() implementation

---
+++ LINQ/Chunks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Chunks
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello Chunks!");
+
+            var arr = new[]
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            var chunks = arr.Split(2);
+
+            var i = 0;
+
+            foreach (var chunk in chunks)
+            {
+                i += 1;
+
+                Console.WriteLine($"Chunk {i}");
+
+                foreach (var item in chunk)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
