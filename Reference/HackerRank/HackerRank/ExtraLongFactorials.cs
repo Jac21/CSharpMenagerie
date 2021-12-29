5491cde818f41b22116f54a04136bcf3b207cf1f
Jeremy Cantu <jcantu521@gmail.com>
2021-12-28 21:34:05 -0600

    Created HackerRank folder under Reference parent folder, solution

---
+++ Reference/HackerRank/HackerRank/ExtraLongFactorials.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace HackerRank
+{
+    public class ExtraLongFactorials
+    {
+        public static BigInteger ComputeExtraLongFactorials(BigInteger n)
+        {
+            if(n == 1)
+            {
+                return 1;
+            }
+
+            return n * ComputeExtraLongFactorials(n - 1);
+        }
+    }
+}
