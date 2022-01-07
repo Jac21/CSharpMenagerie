de0330fb9383a26c598938f259900b2291066d76
Jeremy Cantu <jcantu521@gmail.com>
2022-01-06 20:05:23 -0600

    CarPooler problem and solution and tests

---
+++ Reference/HackerRank/HackerRank.Unit.Tests/TownJudgeFinderTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests;
+
+public class TownJudgeFinderTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TownJudgeFinder_SimpleCase_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCase = TownJudgeFinder.FindJudge(2, new[]
+        {
+            new[] {1, 2}
+        });
+
+        // assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TownJudgeFinder_LargerCase_Success()
+    {
+        // arrange
+
+        // act
+        var largerCase = TownJudgeFinder.FindJudge(3, new[]
+        {
+            new[] {1, 3},
+            new[] {2, 3}
+        });
+
+        // assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TownJudgeFinder_VeryLargeCase_Success()
+    {
+        // arrange
+
+        // act
+        var veryLargeCase = TownJudgeFinder.FindJudge(4, new[]
+        {
+            new[] {1, 3},
+            new[] {1, 4},
+            new[] {2, 3},
+            new[] {2, 4},
+            new[] {4, 3},
+        });
+
+        // assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TownJudgeFinder_NoJudge_Success()
+    {
+        // arrange
+
+        // act
+        var noJudgeCase = TownJudgeFinder.FindJudge(3, new[]
+        {
+            new[] {1, 3},
+            new[] {2, 3},
+            new[] {3, 1},
+        });
+
+        // assert
+        Assert.Pass();
+    }
+}
