5464d998fde9ac95cf971776b7e56b9d205551be
Jeremy Cantu <jcantu521@gmail.com>
2022-01-09 13:02:37 -0600

    Greedy problems, solutions, and tests

---
+++ Reference/HackerRank/HackerRank.Unit.Tests/Greedy/FrequencyOfTheMostFrequentElementTests.cs
@@ -0,0 +1,48 @@
+using HackerRank.Greedy;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.Greedy;
+
+public class FrequencyOfTheMostFrequentElementTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void FrequencyOfTheMostFrequentElement_SimpleCase_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCase = FrequencyOfTheMostFrequentElement.MaxFrequency(new[] {1, 2, 4}, 5);
+
+        // assert
+        Assert.AreEqual(3, simpleCase);
+    }
+
+    [Test]
+    public void FrequencyOfTheMostFrequentElement_SimpleCaseTwo_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCaseTwo = FrequencyOfTheMostFrequentElement.MaxFrequency(new[] {1, 4, 8, 13}, 5);
+
+        // assert
+        Assert.AreEqual(2, simpleCaseTwo);
+    }
+
+    [Test]
+    public void FrequencyOfTheMostFrequentElement_SimpleCaseThree_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCaseThree = FrequencyOfTheMostFrequentElement.MaxFrequency(new[] {3, 9, 6}, 2);
+
+        // assert
+        Assert.AreEqual(1, simpleCaseThree);
+    }
+}
