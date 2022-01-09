807e353d28a33c7be1d40e0a13e20c9ede80270c
Jeremy Cantu <jcantu521@gmail.com>
2022-01-08 19:08:43 -0600

    TwoSum solution and tests

---
+++ Reference/HackerRank/HackerRank.Unit.Tests/TwoSumTests.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests;
+
+public class TwoSumTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TwoSum_SimpleCase_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCase = TwoSum.FindTwoSum(new[]
+        {
+            2, 7, 11, 15
+        }, 9);
+
+        // assert
+        Assert.AreEqual(new[]
+        {
+            0, 1
+        }, simpleCase);
+    }
+
+    [Test]
+    public void TwoSum_SimpleCaseTwo_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCaseTwo = TwoSum.FindTwoSum(new[]
+        {
+            3, 2, 4
+        }, 6);
+
+        // assert
+        Assert.AreEqual(new[]
+        {
+            1, 2
+        }, simpleCaseTwo);
+    }
+
+    [Test]
+    public void TwoSum_BaseCase_Success()
+    {
+        // arrange
+
+        // act
+        var baseCase = TwoSum.FindTwoSum(new[]
+        {
+            3, 3
+        }, 6);
+
+        // assert
+        Assert.AreEqual(new[]
+        {
+            0, 1
+        }, baseCase);
+    }
+}
