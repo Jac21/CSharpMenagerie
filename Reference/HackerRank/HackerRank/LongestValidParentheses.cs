92f44661377fd17b692c2d94018a6aa3a4de2bea
Jeremy Cantu <jcantu521@gmail.com>
2022-01-09 09:28:43 -0600

    Longest valid parentheses solution and tests

---
+++ Reference/HackerRank/HackerRank/LongestValidParentheses.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace HackerRank
+{
+    public static class LongestValidParentheses
+    {
+        public static int FindLongestValidParentheses(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            var max = 0;
+            var extraClosures = -1;
+
+            var openParens = new Stack<int>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] is '(')
+                {
+                    openParens.Push(i);
+                }
+                else if (openParens.Count is 0 || extraClosures > openParens.Peek())
+                {
+                    extraClosures = i;
+                }
+                else
+                {
+                    openParens.Pop();
+
+                    max = Math.Max(max,
+                        openParens.Count is 0 ? i - extraClosures : i - Math.Max(extraClosures, openParens.Peek()));
+                }
+            }
+
+            return max;
+        }
+    }
+}
