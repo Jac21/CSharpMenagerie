d2eddf1d8b6ecb3e5d4625739afc372e65077243
Jeremy Cantu <jcantu521@gmail.com>
2022-01-09 20:42:45 -0600

    Some basic algorithm solutions and tests

---
+++ Reference/HackerRank/HackerRank/Algorithms/FreshPromo.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Algorithms
+{
+    public static class FreshPromo
+    {
+        /*
+         * Complete the 'foo' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. STRING_ARRAY codeList
+         *  2. STRING_ARRAY shoppingCart
+         */
+
+        public static int foo(List<string> codeList, List<string> shoppingCart)
+        {
+            if (!codeList.Any()) return 1;
+
+            if (!shoppingCart.Any()) return 0;
+
+            var isWinner = false;
+
+            foreach (var listGroup in codeList)
+            {
+                var listGroupList = listGroup
+                    .Split(',')
+                    .Select(t => t.Trim())
+                    .ToList();
+
+                var isAnythingIndex =
+                    listGroupList.FindIndex(x => string.Equals(x, "anything", StringComparison.OrdinalIgnoreCase));
+
+                if (isAnythingIndex != -1)
+                {
+                    var uniqueItems = shoppingCart.Distinct();
+
+                    foreach (var uniqueItem in uniqueItems)
+                    {
+                        var newCodeList = listGroupList.ToArray();
+
+                        newCodeList[isAnythingIndex] = uniqueItem;
+
+                        isWinner = !newCodeList.Except(shoppingCart).Any();
+
+                        if (isWinner) break;
+                    }
+                }
+                else
+                {
+                    isWinner = !listGroupList.Except(shoppingCart).Any();
+                }
+            }
+
+            return isWinner ? 1 : 0;
+        }
+    }
+}
