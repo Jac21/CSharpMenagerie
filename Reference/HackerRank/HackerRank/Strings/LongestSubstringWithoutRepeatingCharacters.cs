06dcf1ea29b7cef4ea8a63a6fb6abe65109af332
Jeremy Cantu <jcantu521@gmail.com>
2022-01-15 11:18:29 -0600

    LongestSubstringWithoutRepeatingCharacters.cs solution and tests

---
+++ Reference/HackerRank/HackerRank/Strings/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace HackerRank.Strings
+{
+    public static class LongestSubstringWithoutRepeatingCharacters
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            var counter = int.MinValue;
+            var seenCharacters = new HashSet<char>();
+
+            var left = 0;
+            var i = 0;
+
+            while (i < s.Length)
+            {
+                while (!seenCharacters.Add(s[i]))
+                {
+                    seenCharacters.Remove(s[left]);
+                    left++;
+                }
+
+                counter = Math.Max(counter, i - left + 1);
+                i++;
+            }
+
+            return counter;
+        }
+    }
+}
