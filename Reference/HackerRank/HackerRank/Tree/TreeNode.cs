2f8d2d009116cc1f17fa2bef30080dd1b2517902
Jeremy Cantu <jcantu521@gmail.com>
2022-01-16 15:14:50 -0600

    Greedy and Tree-based problems, solutions and tests

---
+++ Reference/HackerRank/HackerRank/Tree/TreeNode.cs
@@ -0,0 +1,16 @@
+namespace HackerRank.Tree
+{
+    public class TreeNode
+    {
+        public readonly int Val;
+        public readonly TreeNode Left;
+        public readonly TreeNode Right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            Val = val;
+            Left = left;
+            Right = right;
+        }
+    }
+}
