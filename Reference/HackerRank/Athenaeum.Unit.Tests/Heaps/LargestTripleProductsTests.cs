eef10cea696f8d53891fcfd93d01e4d368c495ea
Jeremy Cantu <jcantu521@gmail.com>
2022-03-06 10:04:48 -0600

    Updated C# version target, Largest Triple Product exercise and tests

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/Heaps/LargestTripleProductsTests.cs
@@ -0,0 +1,52 @@
+using Athenaeum.Heaps;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.Heaps;
+
+public class LargestTripleProductsTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void LargestTripleProducts_SimpleCase_Success()
+    {
+        // arrange
+        var input = new[]
+        {
+            1, 2, 3, 4, 5
+        };
+
+        // act
+        var result =
+            LargestTripleProducts.FindMaxProduct(input);
+
+        // assert
+        Assert.AreEqual(new[]
+        {
+            -1, -1, 6, 24, 60
+        }, result);
+    }
+
+    [Test]
+    public void LargestTripleProducts_SimpleCaseTwo_Success()
+    {
+        // arrange
+        var input = new[]
+        {
+            2, 1, 2, 1, 2
+        };
+
+        // act
+        var result =
+            LargestTripleProducts.FindMaxProduct(input);
+
+        // assert
+        Assert.AreEqual(new[]
+        {
+            -1, -1, 4, 4, 8
+        }, result);
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/Heaps/LargestTripleProducts.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Athenaeum.Heaps
+{
+    public static class LargestTripleProducts
+    {
+        public static int[] FindMaxProduct(int[] arr)
+        {
+            if (!arr.Any()) return Array.Empty<int>();
+
+            if (arr.Length == 1) return new[] {-1};
+
+            if (arr.Length == 2) return new[] {-1, -1};
+
+            // results to return
+            var computedArray = new int[arr.Length];
+
+            // poor-man's priority queue
+            var sortedArrayList = new List<int>
+            {
+                arr[0],
+                arr[1],
+                arr[2]
+            };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // base cases
+                if (i is 0 or 1)
+                {
+                    computedArray[i] = -1;
+                }
+                // simply sum all three encountered values
+                else if (i == 2)
+                {
+                    computedArray[i] = arr[i] * arr[i - 1] * arr[i - 2];
+                }
+                else
+                {
+                    // re-heap
+                    sortedArrayList.Add(arr[i]);
+
+                    sortedArrayList.Sort((a, b) => b.CompareTo(a));
+
+                    computedArray[i] = sortedArrayList.FirstOrDefault() *
+                                       sortedArrayList.Skip(1).FirstOrDefault() *
+                                       sortedArrayList.Skip(2).FirstOrDefault();
+                }
+            }
+
+            return computedArray;
+        }
+    }
+}
