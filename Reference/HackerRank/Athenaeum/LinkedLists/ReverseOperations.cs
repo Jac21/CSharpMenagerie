253d31d70b42e35bb9c8e8f5a58f15bbb483c37c
Jeremy Cantu <jcantu521@gmail.com>
2022-03-19 15:52:18 -0500

    Stubs for future exercises

---
+++ Reference/HackerRank/Athenaeum/LinkedLists/ReverseOperations.cs
@@ -0,0 +1,17 @@
+namespace Athenaeum.LinkedLists
+{
+    public class ListNode
+    {
+        public int Data;
+        
+        public ListNode Next;
+    }
+
+    public static class ReverseOperations
+    {
+        public static ListNode Reverse(ListNode head)
+        {
+            return new ListNode();
+        }
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/Recursion/ForeignCurrency.cs
@@ -0,0 +1,11 @@
+namespace Athenaeum.Recursion
+{
+    public static class ForeignCurrency
+    {
+        public static bool CanGetExactChange(int targetMoney, int[] denominations)
+        {
+            // Write your code here
+            return false;
+        }
+    }
+}
