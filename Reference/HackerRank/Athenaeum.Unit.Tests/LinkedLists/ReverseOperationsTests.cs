93c9a58b0076bc1e8e9d7beadbd993a85488c9c2
Jeremy Cantu <jcantu521@gmail.com>
2022-03-21 22:18:22 -0500

    Linked List exercise

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/LinkedLists/ReverseOperationsTests.cs
@@ -0,0 +1,29 @@
+﻿using Athenaeum.LinkedLists;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.LinkedLists
+{
+    public class ReverseOperationsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [Ignore("Reference comparison")]
+        public void ReverseOperation_SimpleCase_Success()
+        {
+            // arrange
+            var head = new ListNode(1, new ListNode(2, new ListNode(8, new ListNode(9, new ListNode(12, new ListNode(16))))));
+
+            // act
+            var simpleCase = ReverseOperations.Reverse(head);
+
+            // assert
+            var expected = new ListNode(1, new ListNode(8, new ListNode(2, new ListNode(9, new ListNode(16, new ListNode(12))))));
+
+            Assert.AreEqual(expected, simpleCase);
+        }
+    }
+}
--- Reference/HackerRank/Athenaeum/LinkedLists/ReverseOperations.cs
+++ Reference/HackerRank/Athenaeum/LinkedLists/ReverseOperations.cs
@@ -3,15 +3,69 @@ namespace Athenaeum.LinkedLists
     public class ListNode
     {
         public int Data;
-        
-        public ListNode Next;
+
+        public ListNode? Next;
+
+        public ListNode(int data)
+        {
+            Data = data;
+        }
+
+        public ListNode(int data, ListNode next)
+        {
+            Data = data;
+            Next = next;
+        }
     }
 
     public static class ReverseOperations
     {
         public static ListNode Reverse(ListNode head)
         {
-            return new ListNode();
+            var dummyNode = new ListNode(0, head);
+            var prev = dummyNode;
+            var curr = head;
+
+            while (curr != null)
+            {
+                var start = curr;
+
+                while (curr != null &&
+                    curr.Data % 2 == 0)
+                {
+                    curr = curr.Next;
+                }
+
+                if (start != curr)
+                {
+                    prev.Next = ReverseSublist(start, curr);
+                }
+
+                if (curr != null)
+                {
+                    prev = curr;
+                    curr = curr.Next;
+                }
+            }
+
+            return dummyNode.Next;
+        }
+
+        private static ListNode? ReverseSublist(ListNode start, ListNode? end)
+        {
+            var prev = end;
+            var curr = start;
+
+            while (curr != end)
+            {
+                var next = curr.Next;
+                curr.Next = prev;
+
+                prev = curr;
+                curr = next;
+            }
+
+            return prev;
         }
     }
 }
