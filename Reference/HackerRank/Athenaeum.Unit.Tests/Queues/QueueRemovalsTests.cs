ce369971828934aa028c8e2e90ca4f2ed565bb45
Jeremy Cantu <jcantu521@gmail.com>
2022-03-21 21:46:02 -0500

    Queue exercises

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/Queues/QueueRemovalsTests.cs
@@ -0,0 +1,31 @@
+﻿using Athenaeum.Queues;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.Queues
+{
+    public class QueueRemovalsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void QueueRemovals_SimpleCase_Success()
+        {
+            // arrange
+            var arr = new[]
+            {
+                1, 2, 2, 3, 4, 5
+            };
+
+            var x = 5;
+
+            // act
+            var simpleCase = QueueRemovals.FindPositions(arr, x);
+
+            // assert
+            Assert.AreEqual(new[] { 5, 6, 4, 1, 2 }, simpleCase);
+        }
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/Queues/QueueRemovals.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Athenaeum.Queues
+{
+    public static class QueueRemovals
+    {
+
+        public static int[] FindPositions(int[] arr, int x)
+        {
+            if (arr.Length == 0 || x == 0) return Array.Empty<int>();
+
+            x = Math.Min(x, arr.Length);
+
+            int[] result = new int[x];
+            int max;
+            int start = 0;
+
+            // O(x)
+            for (int i = 0; i < x; i++)
+            {
+                int count = Math.Min(x, arr.Length - i);
+                int maxElementAt = start;
+
+                max = arr[maxElementAt];
+
+                // O(n)
+                while (count > 0)
+                {
+                    if (arr[start] != -1)
+                    {
+                        if (max < arr[start])
+                        {
+                            max = arr[start];
+                            maxElementAt = start;
+                        }
+
+                        if (arr[start] > 0)
+                        {
+                            arr[start] = arr[start] - 1;
+                        }
+
+                        count -= 1;
+                    }
+
+                    start += 1;
+                    start = start % arr.Length;
+                }
+
+                arr[maxElementAt] = -1;
+
+                result[i] = maxElementAt + 1;
+            }
+
+            return result;
+        }
+    }
+}
