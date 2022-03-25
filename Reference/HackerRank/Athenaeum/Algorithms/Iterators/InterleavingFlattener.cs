f04d2b284ae2fe59aa173261ff120bd09f7fd2f5
Jeremy Cantu <jcantu521@gmail.com>
2022-03-24 21:02:49 -0500

    Interleaving iterator class creation

---
+++ Reference/HackerRank/Athenaeum/Algorithms/Iterators/InterleavingFlattener.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Athenaeum.Algorithms.Iterators
+{
+    /// <summary>
+    /// https://techdevguide.withgoogle.com/resources/former-interview-question-flatten-iterators/
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class InterleavingFlattener<T>
+    {
+        private readonly Queue<IEnumerator<T>> iterqueue;
+
+        public InterleavingFlattener(IEnumerator<T>[] iterlist)
+        {
+            iterqueue = new Queue<IEnumerator<T>>();
+
+            for (var i = 0; i < iterlist.Length; i++)
+            {
+                iterqueue.Enqueue(iterlist[i]);
+            }
+        }
+
+        public T Next()
+        {
+            if (!HasNext()) throw new ArgumentOutOfRangeException();
+
+            var iterator = iterqueue.Dequeue();
+
+            var val = iterator.Current;
+
+            if (iterator.MoveNext()) iterqueue.Enqueue(iterator);
+
+            return val;
+        }
+
+        public bool HasNext()
+        {
+            return iterqueue.Any();
+        }
+    }
+}
