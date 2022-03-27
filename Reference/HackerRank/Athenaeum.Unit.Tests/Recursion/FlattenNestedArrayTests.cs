89b851f5c386484ac42e3c426ed58c36bc6c7dc2
Jeremy Cantu <jcantu521@gmail.com>
2022-03-27 12:24:43 -0500

    Nested array flattening exercises

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/Recursion/FlattenNestedArrayTests.cs
@@ -0,0 +1,82 @@
+﻿using Athenaeum.Recursion;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.Recursion
+{
+    public class FlattenNestedArrayTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FlattenNestedArray_SimpleCase_Success()
+        {
+            // arrange
+            var source = new object[] {
+                "foo", 2, new int[] { 1, 2, 3, 4 }, "bar" };
+
+            // act
+            var simpleCase = FlattenNestedArray.FlattenArray(source);
+
+            // assert
+            Assert.AreEqual(new object[] { "foo", 2, 1, 2, 3, 4, "bar" }, simpleCase);
+        }
+
+        [Test]
+        public void FlattenNestedArrayRecursive_SimpleCase_Success()
+        {
+            // arrange
+            var source = new object[] {
+                "foo", 2, new int[] { 1, 2, 3, 4 }, "bar" };
+
+            // act
+            var simpleCase = FlattenNestedArray.FlattenNestedArrayRecursive(source);
+
+            // assert
+            Assert.AreEqual(new object[] { "foo", 2, 1, 2, 3, 4, "bar" }, simpleCase);
+        }
+
+        [Test]
+        public void FlattenNestedIntegerArrayRecursive_SimpleCase_Success()
+        {
+            // arrange
+            var source = new object[]
+            {
+                1, 8, new object[] {6, 2, new int[] {9}, 8}, 2, 6
+            };
+
+            // act
+            var simpleCase = FlattenNestedArray.FlattenNestedArrayRecursive(source);
+
+            // assert
+            Assert.AreEqual(new object[] { 1, 8, 6, 2, 9, 8, 2, 6 }, simpleCase);
+        }
+    }
+
+    public class FlattenExtensionsTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FlattenExtensions_SimpleCase_Success()
+        {
+            // arrange
+            var source = new object[]
+            {
+                1, 8, new object[] {6, 2, new object[] {9}, 8}, 2, 6
+            };
+
+            // act
+            var simpleCase = FlattenExtensions.Flatten(source);
+
+            // assert
+            Assert.AreEqual(new object[] { 1, 8, 6, 2, 9, 8, 2, 6 }, simpleCase);
+        }
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/Recursion/FlattenNestedArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Athenaeum.Recursion
+{
+    public static class FlattenNestedArray
+    {
+        public static IEnumerable<object> FlattenArray(IEnumerable<object> source)
+        {
+            return source
+                .SelectMany(x => x is Array ?
+                    ((IEnumerable)x).Cast<object>() : Enumerable.Repeat(x, 1));
+        }
+
+        public static IEnumerable<object> FlattenNestedArrayRecursive(IEnumerable<object> source)
+        {
+            static IEnumerable<object> flatten(IEnumerable<object> s) => s.SelectMany(x =>
+              {
+                  return x is Array ? flatten(((IEnumerable)x).Cast<object>()) : Enumerable.Repeat(x, 1);
+              });
+
+            return flatten(source);
+        }
+    }
+
+    public static class FlattenExtensions
+    {
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<T> @this)
+        {
+
+            foreach (var item in @this)
+            {
+                if (item is IEnumerable<T> enumerable)
+                {
+                    foreach (var subitem in Flatten(enumerable))
+                    {
+                        yield return subitem;
+                    }
+                }
+
+                else yield return item;
+            }
+        }
+    }
+}
