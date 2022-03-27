73bf2fb577b6c3bbc2c86f66606e7a8edad9b302
Jeremy Cantu <jcantu521@gmail.com>
2022-03-27 11:46:20 -0500

    Coin change dynamic programming problem

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/DynamicProgramming/CoinChangeTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Athenaeum.DynamicProgramming;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.DynamicProgramming
+{
+    public class CoinChangeTests
+    {
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CoinChange_SimpleCase_Success()
+        {
+            // arrange
+
+            // act
+            var simpleCase = CoinChange.MakeCoinChange(new[] { 1, 2, 5 }, 11);
+
+            // assert
+            Assert.AreEqual(3, simpleCase);
+        }
+
+        [Test]
+        public void CoinChange_SimpleCaseTwo_Success()
+        {
+            // arrange
+
+            // act
+            var simpleCase = CoinChange.MakeCoinChange(new[] { 2 }, 3);
+
+            // assert
+            Assert.AreEqual(-1, simpleCase);
+        }
+
+        [Test]
+        public void CoinChange_SimpleCaseThree_Success()
+        {
+            // arrange
+
+            // act
+            var simpleCase = CoinChange.MakeCoinChange(new[] { 1 }, 0);
+
+            // assert
+            Assert.AreEqual(0, simpleCase);
+        }
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/DynamicProgramming/CoinChange.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Athenaeum.DynamicProgramming
+{
+    public class CoinChange
+    {
+        public static int MakeCoinChange(int[] coins, int amount)
+        {
+            if (coins.Length == 0 || amount == 0) return 0;
+
+            // create inclusive array for full amount
+            var dp = new int[amount + 1];
+
+            Array.Fill(dp, -1);
+
+            for (int i = 1; i <= amount; i++)
+            {
+                foreach (int coin in coins)
+                {
+                    // if coin matches i, we only need
+                    // one coin for that amount
+                    if (coin == i)
+                    {
+                        dp[i] = 1;
+                    }
+                    // more likely case, coin is less than i
+                    else if (coin < i)
+                    {
+                        // two cases:
+                        // 1. dp[i] already has solution, and it's worse
+                        // 2. dp[i] does not have a solution yet
+                        if (dp[i - coin] != -1 &&
+                            ((dp[i] != -1 && dp[i] > dp[i - coin] + 1)
+                            || dp[i] == -1))
+                        {
+                            // set solution, take amount of coins, add one more coin
+                            dp[i] = dp[i - coin] + 1;
+                        }
+                    }
+                }
+            }
+
+            return dp[amount];
+        }
+    }
+}
