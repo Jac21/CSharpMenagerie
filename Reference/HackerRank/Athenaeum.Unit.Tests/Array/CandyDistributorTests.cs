afea63532ac82fd951401c57f173a98f4d152b7f
Jeremy Cantu <jcantu521@gmail.com>
2022-03-28 10:25:54 -0500

    Array exercise and tests

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/Array/CandyDistributorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Athenaeum.Arrays;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.Array
+{
+    public class CandyDistributorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CandyDistributor_SimpleCase_Success()
+        {
+            // arrange
+
+            // act
+            var simpleCase = CandyDistributor.DistributeCandies(new[] { 1, 1, 2, 2, 3, 3 });
+
+            // assert
+            Assert.AreEqual(simpleCase, 3);
+        }
+
+        [Test]
+        public void CandyDistributor_SimpleCaseTwo_Success()
+        {
+            // arrange
+
+            // act
+            var simpleCase = CandyDistributor.DistributeCandies(new[] { 1, 1, 2, 3 });
+
+            // assert
+            Assert.AreEqual(simpleCase, 2);
+        }
+
+        [Test]
+        public void CandyDistributor_SimpleCaseThree_Success()
+        {
+            // arrange
+
+            // act
+            var simpleCase = CandyDistributor.DistributeCandies(new[] { 6, 6, 6, 6 });
+
+            // assert
+            Assert.AreEqual(simpleCase, 1);
+        }
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/Arrays/CandyDistributor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Athenaeum.Arrays
+{
+    public static class CandyDistributor
+    {
+        public static int DistributeCandies(int[] candyType)
+        {
+            if (candyType.Length == 0) return 0;
+
+            var set = new HashSet<int>(candyType);
+
+            if (set.Count == 1) return set.Count;
+
+            if (candyType.Length % 2 == 0 &&
+                candyType.Length / 2 <= set.Count)
+            {
+                return candyType.Length / 2;
+            }
+
+            return set.Count;
+        }
+    }
+}
