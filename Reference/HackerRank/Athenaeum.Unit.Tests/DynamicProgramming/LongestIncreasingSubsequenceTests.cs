554e5616fd1aa4a550e61b2ef7ff6da4fb31e9a4
Jeremy Cantu <jcantu521@gmail.com>
2022-03-28 13:12:33 -0500

    Dynamic Programming exercise and tests

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/DynamicProgramming/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,51 @@
+﻿using Athenaeum.DynamicProgramming;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.DynamicProgramming
+{
+    public class LongestIncreasingSubsequenceTests
+    {
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LongestIncreasingSubsequence_SimpleCase_Success()
+        {
+            // arrange
+            var nums = new[] { 10, 9, 2, 5, 3, 7, 101, 18 };
+
+            // act
+            var simpleCase = LongestIncreasingSubsequence.LengthOfLIS(nums);
+
+            // assert
+            Assert.AreEqual(4, simpleCase);
+        }
+
+        [Test]
+        public void LongestIncreasingSubsequence_SimpleCaseTwo_Success()
+        {
+            // arrange
+            var nums = new[] { 0, 1, 0, 3, 2, 3 };
+
+            // act
+            var simpleCase = LongestIncreasingSubsequence.LengthOfLIS(nums);
+
+            // assert
+            Assert.AreEqual(4, simpleCase);
+        }
+
+        [Test]
+        public void LongestIncreasingSubsequence_SimpleCaseThree_Success()
+        {
+            // arrange
+            var nums = new[] { 7, 7, 7, 7, 7, 7, 7 };
+
+            // act
+            var simpleCase = LongestIncreasingSubsequence.LengthOfLIS(nums);
+
+            // assert
+            Assert.AreEqual(1, simpleCase);
+        }
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/DynamicProgramming/LongestIncreasingSubsequence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Athenaeum.DynamicProgramming
+{
+    /// <summary>
+    /// Time: O(n^2)
+    /// Space: O(n)
+    /// </summary>
+    public static class LongestIncreasingSubsequence
+    {
+        public static int LengthOfLIS(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            var dpArray = new int[nums.Length];
+            Array.Fill(dpArray, 1);
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        dpArray[i] = Math.Max(dpArray[i], dpArray[j] + 1);
+                    }
+                }
+            }
+
+            return dpArray.Max();
+        }
+    }
+}
