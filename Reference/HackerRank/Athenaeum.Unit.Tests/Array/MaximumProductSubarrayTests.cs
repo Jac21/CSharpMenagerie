c99dda3217375549dac7290188dd0854aa4f5988
Jeremy Cantu <jcantu521@gmail.com>
2022-09-10 12:38:50 -0500

    Maximum Product Subarray implementation and tests

---
+++ Reference/HackerRank/Athenaeum.Unit.Tests/Array/MaximumProductSubarrayTests.cs
@@ -0,0 +1,36 @@
+using Athenaeum.Arrays;
+using NUnit.Framework;
+
+namespace HackerRank.Unit.Tests.Array;
+
+public class MaximumProductSubarrayTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void MaximumProductSubarray_SimpleCase_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCase = MaximumProductSubarray.MaxProduct(new[] {2, 3, -2, 4});
+
+        // assert
+        Assert.AreEqual(simpleCase, 6);
+    }
+
+    [Test]
+    public void MaximumProductSubarray_SimpleCaseTwo_Success()
+    {
+        // arrange
+
+        // act
+        var simpleCase = MaximumProductSubarray.MaxProduct(new[] {-2, 0, -1});
+
+        // assert
+        Assert.AreEqual(simpleCase, 0);
+    }
+}
---
+++ Reference/HackerRank/Athenaeum/Arrays/MaximumProductSubarray.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+
+namespace Athenaeum.Arrays
+{
+    public static class MaximumProductSubarray
+    {
+        public static int MaxProduct(int[] nums)
+        {
+            switch (nums.Length)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return nums.FirstOrDefault();
+            }
+
+            var firstEntry = nums.FirstOrDefault();
+
+            var result = firstEntry;
+
+            var currentMinimum = firstEntry;
+            var currentMaximum = firstEntry;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                {
+                    (currentMaximum, currentMinimum) = (currentMinimum, currentMaximum);
+                }
+
+                currentMaximum = Math.Max(nums[i], currentMaximum * nums[i]);
+                currentMinimum = Math.Min(nums[i], currentMinimum * nums[i]);
+
+                result = Math.Max(result, currentMaximum);
+            }
+
+            return result;
+        }
+    }
+}
